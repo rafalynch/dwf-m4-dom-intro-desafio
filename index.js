b29553cebad6599dcd453b69e3283392b07df08c
Rafa Lynch <rafa_lynch@hotmail.com>
2021-05-21 13:42:22 -0300

    first commit

--- index.js
+++ index.js
@@ -25,6 +25,22 @@ const cosasQueAprendimos = [
   },
 ];
 
-function main() {}
+function main() {
+  const elLista = document.querySelectorAll(".lista li");
+  elLista.forEach((el) => {
+    el.remove();
+  });
+
+  const listaEL = document.querySelector(".lista");
+
+  for (let item of cosasQueAprendimos) {
+    const listElement = document.createElement("li");
+    listElement.innerHTML = item.tema;
+    const claseAAsignar = item.class || "cosaAprendida";
+    listElement.classList.add(claseAAsignar);
+
+    listaEL.appendChild(listElement);
+  }
+}
 
 main();
